c92efe8f47ef126016e39a62a8a10ac1fbc362fc
Ernesto Bossi <bossi.ernestog@gmail.com>
2017-01-24 16:59:49 -0300

    Add intro actors code

---
+++ intro_actors/lib/intro_actors.ex
@@ -0,0 +1,23 @@
+defmodule Jose do
+  def start do
+    spawn_link(fn -> loop end)
+#   Task.start_link (fn -> loop end)
+  end
+
+  def loop do
+    receive do
+      {pid, :camina} -> IO.puts 'Jose empieza a caminar. Proceso #{inspect self}'
+      {pid, :corre} -> IO.puts 'Ni ahi. Proceso #{inspect self}'
+      {pid, _ } -> send pid, {:error, 'Accion Invalida de #{inspect pid}'}
+    end
+    loop
+  end
+
+end
+
+jose = Jose.start
+send jose, {self,:camina}
+
+Process.register jose, :jose
+
+send :jose, {self, :corre}
