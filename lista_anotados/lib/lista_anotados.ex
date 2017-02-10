b673863e76c3d6303b62f36a0ebb27423d4ef85b
Ernesto Bossi <bossi.ernestog@gmail.com>
2017-02-10 13:09:18 -0300

    Add mix project for today presentation

---
+++ lista_anotados/lib/lista_anotados.ex
@@ -0,0 +1,18 @@
+defmodule ListaAnotados do
+  @moduledoc """
+  Documentation for ListaAnotados.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ListaAnotados.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
